0ed3e165e0403a5b2627f7711bf06a1ac45e5039
Brian Lampe <brian.l.lampe@gmail.com>
2018-11-08 11:01:00 -0800

    Finished blog file process code

--- build/process-blog-files.js
+++ build/process-blog-files.js
@@ -13,16 +13,48 @@ fs.readdirSync('static/blog')
 
   })
 
+const files = []
 
 fs.readdirSync('blog/')
   .filter(f => f.endsWith('.md'))
   .forEach(f => {
+    const fNmNoExt = f.substring(0, f.length - 3)
     const fp = path.resolve('blog', f)
-    const op = path.resolve('static/blog', f.substring(0, f.length - 3) + '.blog.html')
+    const op = path.resolve('static/blog', fNmNoExt + '.blog.html')
     console.log(chalk.cyan(`  Processing file ${fp}`))
     const text = fs.readFileSync(fp, 'utf-8')
     const html = md.render(text)
     console.log(chalk.cyan(`  Writing file ${op}`))
     fs.writeFileSync(op, html)
+    console.log(chalk.cyan('  Adding metadata'));
+
+    var title = fNmNoExt //default to name
+    const hashIdx = text.indexOf('#')
+    if (hashIdx > -1) {
+      const nlIdx = text.indexOf('\n', hashIdx)
+      title = text.substring(hashIdx + 1, nlIdx).trim()
+    }
+
+    console.log(chalk.cyan(`  Titled: ${title}`))
+
+    const stat = fs.statSync(fp)
+
+    const birthtime = stat.birthtime
+
+    const metadata = {
+      'title': title,
+      'createdDate': birthtime,
+      'fileName': f
+    }
+
+    files.push(metadata)
+
 
   })
+
+const mp = path.resolve('static/blog', 'metadata.json')
+
+console.log(chalk.cyan(`  Writing metadata file ${mp}`))
+
+fs.writeFileSync(mp, JSON.stringify(files))
+
